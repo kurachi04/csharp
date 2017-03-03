54d345eb08ea56f0e2886002eff3c50989e4a145
kurachi04 <kurachi04777@gmail.com>
2017-03-03 22:18:56 +0800

    add pokemon class

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -11,6 +11,13 @@ namespace csharp
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World.");
+            pokemon pokemon1 = new pokemon(); //新增一隻寶可夢
+            pokemon1.Height = 2.0f; //f=float
+            pokemon1.Weight = 12.7f;
+            pokemon1.Category = "Lizard";
+            pokemon1.Abilities = "Blaze";
+            pokemon1.Gender = "M";
+            Console.WriteLine("身高:{0},體重{1}", pokemon1.Height, pokemon1.Weight);
         }
     }
 }
