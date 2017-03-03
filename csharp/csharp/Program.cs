039ad87205de15ecad783335949758fe9dc61eb7
kurachi04 <kurachi04777@gmail.com>
2017-03-03 20:46:57 +0800

    練習Hello World

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace csharp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Hello World.");
         }
     }
 }
